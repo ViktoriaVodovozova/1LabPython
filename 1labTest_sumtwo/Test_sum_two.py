34fcdae4e4bfc29cf573f828e95f5126dd228f1e
ViktoriaVodovozova <Vikktoriaa2508@mail.ru>
2025-09-15 21:07:55 +0300

    first commit
    first commit

---
+++ 1labTest_sumtwo/Test_sum_two.py
@@ -0,0 +1,47 @@
+import unittest
+
+from main import sum_two
+
+
+class Testsum_two(unittest.TestCase):
+    def test_correct_from_example(self):
+        self.assertEqual(sum_two([2, 7, 11, 15], 9), [0, 1])
+
+        self.assertEqual(sum_two([3, 2, 4], 6), [1, 2])
+
+        self.assertEqual(sum_two([3, 3], 6), [0, 1])
+
+    def test_correct_negative_num(self):
+        self.assertEqual(sum_two([-4, -3, -2], -5), [1, 2])
+
+    def test_correct_huge_num(self):
+        self.assertEqual(sum_two([100000, 400000, 4, 210000], 400004), [1, 2])
+
+
+    def test_empty(self):
+        with self.assertRaises(TypeError):
+            sum_two([], 56)
+
+    def test_targ_zero(self):
+        with self.assertRaises(TypeError):
+            sum_two([1, 2, 3], 0)
+
+    def test_integrity_target(self):
+        with self.assertRaises(TypeError):
+            sum_two([1, 3, 7], 12.56)
+
+    def test_integrity(self):
+        with self.assertRaises(TypeError):
+            sum_two([1.23, 1.56, 90.7], 10)
+
+    def test_str_in_num(self):
+        with self.assertRaises(TypeError):
+            sum_two([1, 3, "8"], 4)
+
+    def test_no_sum(self):
+        with self.assertRaises(TypeError):
+            sum_two([1, 15, 9, 37], 17)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ 1lab_sum_two/1labcode_sum_two.py
@@ -0,0 +1,21 @@
+def sum_two(nums, target):
+    if len(nums) == 0:
+        raise TypeError("Пусто")
+    if target == 0:
+        raise TypeError("Таргет равен нулю")
+    if int(target) != target:
+        raise TypeError("Таргет - нецелое число")
+    for x in nums:
+        if int(x) != x:
+            raise TypeError("В списке есть нецелое число")
+        if str(x) == x:
+            raise TypeError("В списке есть строка")
+    res = []
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)): 
+            if nums[i] + nums[j] == target:
+                res.append([i, j])
+                return res[0] # берем только пару с наименьшими индексами
+    raise TypeError("Нету суммы таргета")
+
+
