d4bd778b4474bd01d37e0c2af54686ebb4bb8120
ViktoriaVodovozova <Vikktoriaa2508@mail.ru>
2025-10-09 22:10:38 +0300

    first commit
    first commit

---
+++ Laboratory3/test_tree_3lab.py
@@ -0,0 +1,40 @@
+import unittest
+
+from tree import gen_bin_tree
+
+
+class TestMath(unittest.TestCase):
+    def test_another_tree(self):
+        self.assertEqual(gen_bin_tree(3, 7, lambda x: x * 2, lambda y: y + 1),
+                         {'7': [{'14': [{'28': []}, {'15': []}]}, {'8': [{'16': []}, {'9': []}]}]})
+
+    def test_float_main_root(self):
+        self.assertEqual(gen_bin_tree(3, 7.7, lambda x: x + 1, lambda y: y + 2),
+                         {'7.7': [{'8.7': [{'9.7': []}, {'10.7': []}]},
+                                  {'9.7': [{'10.7': []}, {'11.7': []}]}]})
+
+    def test_float_both_roots(self):
+        self.assertEqual(gen_bin_tree(3, 7, lambda x: x + 2.1, lambda y: y + 1.2),
+                         {'7': [{'9.1': [{'11.2': []}, {'10.299999999999999': []}]},
+                                {'8.2': [{'10.299999999999999': []}, {'9.399999999999999': []}]}]})
+
+    def test_height_zero(self):
+        self.assertEqual(gen_bin_tree(0, 7, lambda x: x + 1, lambda y: y + 2),
+                         {'7': []})
+
+    def test_negativ_op(self):
+        self.assertEqual(gen_bin_tree(3, 100, lambda x: x - 1, lambda y: y // 2),
+                         {'100': [{'99': [{'98': []}, {'49': []}]},
+                                  {'50': [{'49': []}, {'25': []}]}]})
+
+    def test_float_height(self):
+        with self.assertRaises(TypeError):
+            gen_bin_tree(3.7, 7, lambda x: x + 1, lambda y: y + 2)
+
+    def test_str_height(self):
+        with self.assertRaises(TypeError):
+            gen_bin_tree("3", 7, lambda x: x + 1, lambda y: y + 2)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ Laboratory3/tree_3lab.py
@@ -0,0 +1,35 @@
+def gen_bin_tree(height: int = 0, root=4, l_b=lambda x: x, r_b=lambda y: y):
+    """
+        Генерирует бинарное дерево в виде словаря.
+
+        Args:
+            height: Высота дерева (целое число)
+            root: Корневое значение
+            l_b: Функция для левого потомка
+            r_b: Функция для правого потомка
+
+        Returns:
+            Словарь вида {корень: [левое_поддерево, правое_поддерево]}
+    """
+    if int(height) != height:
+        raise TypeError("Введено нецелое количесество этажей")
+    if str(height) == height:
+        raise TypeError("Введено строковое представление числа")
+    if height <= 1:
+        return {str(root): []}
+    return {str(root): [gen_bin_tree(height - 1, l_b(root), l_b, r_b),
+                        gen_bin_tree(height - 1, r_b(root), l_b, r_b)]}
+
+
+def main():
+    """
+    Основная функция для демонстрации работы моего варианта gen_bin_tre.
+    left_branch = root * 4
+    right_branch = root + 1
+    """
+    gen_bin_tree(4, 4, lambda x: x * 4, lambda y: y + 1)
+    print(gen_bin_tree(4, 4, lambda x: x * 4, lambda y: y + 1))
+
+
+if __name__ == "__main__":
+    main()
